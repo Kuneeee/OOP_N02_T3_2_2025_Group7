60a6a5c6c635cd98649f58c8420114a2ec3d611b
Trần Quang Trường <24100073@st.phenikaa-uni.edu.vn>
2025-06-23 10:50:13 +0000

    k

---
+++ quanLyBanHang/test/test.java
@@ -0,0 +1,17 @@
+class Number {
+    int i;
+}
+public class test {
+}
+
+class PassObject {
+    static void f(Number m) {
+        m.i = 15;
+    }
+    public static void main(String[] args) {
+        Number n = new Number();
+        n.i = 14;
+        PassObject.f(n);
+        System.out.println(n.i);
+    }
+}
