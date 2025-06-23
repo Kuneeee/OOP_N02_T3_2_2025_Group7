3c34906c8bdd1937286e9ec2df07b0403fd4b278
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-06-23 09:45:09 +0000

    l

--- quanLyBanHang/src/ban.java
+++ quanLyBanHang/src/ban.java
@@ -2,7 +2,6 @@ public class ban
 {
    public static void main(String[] args) 
    {
-       //iam here
-      // i want to see u
+      
    }
 }
--- quanLyBanHang/src/doanhThu.java
+++ quanLyBanHang/src/doanhThu.java
@@ -1,6 +1,5 @@
 public class doanhThu
 {
-   //hello world;
    
    public static void main(String[] args) 
    {
