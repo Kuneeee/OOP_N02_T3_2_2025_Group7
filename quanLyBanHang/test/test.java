674ac27532f2ac7f42dddf5ebe14dad4d886c52f
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-06-23 10:52:15 +0000

    long

--- quanLyBanHang/test/test.java
+++ quanLyBanHang/test/test.java
@@ -1,7 +1,15 @@
-public class test
-{
-    public static void main(String[] args)
-    {
-        System.out.println("hello");
+class Number {
+        int i;
+    }
+public class test {
+    static void f(Number m) {
+        m.i = 15;
+    }
+    public static void main(String[] args) {
+        Number n = new Number();
+        n.i = 14;
+        test.f(n);
+        System.out.println(n.i);
     }
 }
+
