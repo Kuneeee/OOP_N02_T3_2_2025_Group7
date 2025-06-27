481d01b3076f62c271cfaa5e08f172396e24ec85
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-06-27 10:46:32 +0000

    change app.java

--- quanLyBanHang/src/app.java
+++ quanLyBanHang/src/app.java
@@ -1,8 +1,21 @@
+import testcase.breakAndContinue;
+import testcase.passObject;
+import testcase.number;
+import testcase.randNum;
+
 public class app
 {
    public static void main(String[] args) 
    {
-      
+      breakAndContinue.test();
+
+      number n = new number();
+      passObject.f(n);
+      System.out.println(n.i);
+
+      int a;
+      a = randNum.random();
+      System.out.println("random number " + a);
    }
 }
 
