045783b596433ac4f194754589f9f5f3c8eefc16
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-06-30 09:46:36 +0000

    Update app.java and hangHoa.java

--- quanLyBanHang/src/doanhThu.java
+++ quanLyBanHang/src/doanhThu.java
@@ -1,7 +1,7 @@
 public class doanhThu
 {
    
-   public static void main(String[] args) 
+   public static void test2(String[] args) 
    {
        
    }
--- quanLyBanHang/src/nhap.java
+++ quanLyBanHang/src/nhap.java
@@ -1,6 +1,6 @@
 public class nhap
 {
-   public static void main(String[] args) 
+   public static void test4(String[] args) 
    {
        
    }
