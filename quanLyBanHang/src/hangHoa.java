3d2db3e4e35b527438bda55ea4a2919850ade41f
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-06-30 09:38:37 +0000

    Update hangHoa.java

--- quanLyBanHang/src/hangHoa.java
+++ quanLyBanHang/src/hangHoa.java
@@ -1,12 +1,18 @@
 public class hangHoa
 {
-   public String hang_hoa_ID;
+   public int hang_hoa_ID;
    public String ten_hang_hoa;
    public String nha_sx;
-   public int nam_sx;
    public String loai_hang_hoa;
-   public static void main(String[] args) 
+   public int nam_sx;
+   hangHoa(){};
+
+   public hangHoa(int hhi, String thh, String nsx, String lhh, int namsx)
    {
-       
+      hang_hoa_ID = hhi;
+      ten_hang_hoa = thh;
+      nha_sx = nsx;
+      loai_hang_hoa = lhh;
+      nam_sx = namsx;
    }
 }
