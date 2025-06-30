fcf277ef682f1c5e56e10df6681c60b05ea780dd
Nguyen Gia Bao <24100074@st.phenikaa-uni.edu.vn>
2025-06-30 11:16:34 +0000

    Update Persoon

--- quanLyBanHang/testcase/Person.java
+++ quanLyBanHang/testcase/Person.java
@@ -2,6 +2,6 @@ public class Person{
     private String name;
     public String getName(){
         return name;}
-    public void setName(string newName){
-        this.name =newName;}
+    public void setName(String newName){
+        this.name = newName;}
     }
