902035a6c00e1aa9c890097d834a7173ab90d40e
Nguyen Gia Bao <24100074@st.phenikaa-uni.edu.vn>
2025-07-17 07:02:29 +0000

    bai tap

---
+++ quanLyBanHang/review/NameNumber.java
@@ -0,0 +1,15 @@
+public class NameNumber {
+    private String lastName;
+    private String telName;
+    public NameNumber() {}
+    public NameNumber(String name,String num) {
+        lastName = name;
+        telName = num;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getTelName() {
+        return telName;
+    }
+}
