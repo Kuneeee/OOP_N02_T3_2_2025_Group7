7ace2ec1d4e6361a31f90375281f050fca2f8574
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-07-18 10:09:50 +0000

    new update classes

---
+++ quanLyBanHang/src/App.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class App {
+   // Main chạy chương trình
+   public static void main(String[] args) 
+   {
+      // Test code
+      /*
+      Scanner input = new Scanner(System.in);
+
+      breakAndContinue.test();
+
+      number n = new number();
+      passObject.f(n);
+      System.out.println(n.i);
+
+      int a;
+      a = randNum.RandNum();
+      System.out.println("random number " + a);
+
+      int b = input.nextInt();
+      if (shortCircuit.test1(b) == true) System.out.println("be hon 10");
+      else if (shortCircuit.test2(b) == true) System.out.println("be hon 20");
+      else if (shortCircuit.test3(b) == true) System.out.println("be hon 30");
+      input.close();
+
+      switchCake.test();
+      
+      hangHoa myObj = new hangHoa(1, "banh mi", "long dep zai", 2025, "do an");
+      System.out.println("hang hoa id: " + myObj.hang_hoa_ID);
+      System.out.println("ten hang hoa: " + myObj.ten_hang_hoa);
+      System.out.println("nha san xuat: " + myObj.nha_sx);
+      System.out.println("loai hang hoa: " + myObj.loai_hang_hoa);
+      System.out.println("nam san xuat: " + myObj.nam_sx);
+      */
+      // Test code
+
+
+
+      // Gọi các phương thức để nhập, cập nhật và xuất hàng hóa
+      //nhapHang(hanghoaID, tenHangHoa, soLuongNhap, giaNhap, ngayNhap);
+      //capNhatHang(hanghoaID, tenHangHoa, soLuongNhap, ngayNhap, soLuongNhap, tenHangHoa);
+      //xuatHang(hanghoaID, tenHangHoa, soLuongNhap, soLuongNhap, ngayNhap);
+
+      // test
+         
+   }
+}
