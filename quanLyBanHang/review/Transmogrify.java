bc6211a93a311b9180bde2656ce50a09924d0698
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-07-21 10:28:20 +0000

    Big update

--- quanLyBanHang/review/Transmogrify.java
+++ quanLyBanHang/review/Transmogrify.java
@@ -19,7 +19,7 @@ class Stage {
 public class Transmogrify {
     public static void main(String[] args){
         Stage s = new Stage();
-        s.go(); //happy actor
+        s.go(); // happy actor
         s.change();
         s.go(); // sad actor
     }
