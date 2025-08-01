72842d532e32a06082c3903342a536ebe506d432
Đặng Đức Long <24100068@st.phenikaa-uni.edu.vn>
2025-08-01 09:23:16 +0000

    change folder

--- quanLyBanHang/src/App.java
+++ quanLyBanHang/src/App.java
@@ -1,6 +1,11 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+@SpringBootApplication
 public class App {
    // Main chạy chương trình
    public static void main(String[] args) 
